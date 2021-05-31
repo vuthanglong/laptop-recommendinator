c7a7e078d90c4009d573e79e47ff24ddb6c69c5b
longthangvu <vuthanglonghd@gmail.com>
2021-05-31 17:56:14 +0700

    fix response on client side

--- templates/assets/js/socket.js
+++ templates/assets/js/socket.js
@@ -2,16 +2,15 @@ export function prepareSocket() {
   var socket = io.connect(
     window.location.protocol + '//' + document.domain + ':' + location.port
   )
-
+  var prefix = 'https://laptop88.vn/'
   socket.on('connect', function () {
     socket.emit('connected', {
       data: 'User Connected',
       session_id: socket.id,
     })
-    $('form').click('submit', function (e) {
+    $('#send_icon').click('submit', function (e) {
       e.preventDefault()
       let message = $('input#message').val()
-      console.log($('input#message').val())
       if (message)
         socket.emit('my event', {
           message,
@@ -21,25 +20,41 @@ export function prepareSocket() {
     })
   })
   socket.on('my response', function (msg) {
-    console.log(msg)
     if (typeof msg.message !== 'undefined') {
       $('#messages_display').append(`
-        <div class="message message_client">
+      <div class="message message_client">
           <div class="message_content">${msg.message}</div>
           <div class="message_time">${msg.time}</div>
         </div>
-    `)
+        `)
     }
     scrollToEnd()
   })
   socket.on('bot response', function (msg) {
     if (typeof msg.response !== 'undefined') {
-      $('#messages_display').append(`
+      if (!msg.response[1] && !msg.response[2])
+        $('#messages_display').append(`
         <div class="message message_AI">
-          <div class="message_content">${msg.response}</div>
+          <div class="message_content">${msg.response[0]}</div>
           <div class="message_time">${msg.time}</div>
         </div>
     `)
+      else {
+        $('#messages_display').append(`
+            <div class="message message_AI">
+              <div class="message_content">${msg.response[0]}: </br>
+              </div>
+              <div class="message_time">${msg.time}</div>
+              </div>
+              `)
+        msg.response[1].forEach(function (res, i) {
+          $('#messages_display .message_AI .message_content').append(
+            `<a href='${
+              prefix + msg.response[2][i]
+            }' target='_blank'>- ${res}</a> </br>`
+          )
+        })
+      }
     }
     scrollToEnd()
   })
